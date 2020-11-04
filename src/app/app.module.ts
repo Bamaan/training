cc36633b7301d6c4df7191b8a9cc07c766fba130
Gregoor van Diepen <gregoorvandiepen@gmail.com>
2020-11-04 11:26:48 +0100

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { AppState } from './app.state';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     NgxsModule.forRoot([AppState], {
       developmentMode: !environment.production
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
