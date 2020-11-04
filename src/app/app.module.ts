6a7d1fa238431474ea6a4f0b363497ad2adcfc6a
Gregoor van Diepen <gregoorvandiepen@gmail.com>
2020-11-04 11:16:15 +0100

    added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { AppState } from './app.state';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
